11fa66990457c3e6b103bae1a46a3842007d02d0
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-01-18 03:34:44 +0900

    fix: ActivityResponse id 추가

--- src/main/java/com/cotato/_th_cokathon/team3/api/dto/response/ActivityResponse.java
+++ src/main/java/com/cotato/_th_cokathon/team3/api/dto/response/ActivityResponse.java
@@ -3,6 +3,7 @@ package com.cotato._th_cokathon.team3.api.dto.response;
 import com.cotato._th_cokathon.team3.domain.entity.Activity;
 
 public record ActivityResponse(
+	Long id,
 	String title,
 	String imageUrl,
 	Long participantCount
@@ -10,6 +11,7 @@ public record ActivityResponse(
 
 	public static ActivityResponse from(Activity activity) {
 		return new ActivityResponse(
+			activity.getId(),
 			activity.getTitle(),
 			activity.getImageUrl(),
 			activity.getParticipantCount()
