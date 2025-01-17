a51acfc6136391f9715b82b5ce33e75ffec03b4b
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-01-18 02:45:16 +0900

    chore: Swagger Cors config

--- src/main/java/com/cotato/_th_cokathon/team3/common/config/WebConfig.java
+++ src/main/java/com/cotato/_th_cokathon/team3/common/config/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "*")
+			.allowedOrigins("http://localhost:3000")
+			.allowedOrigins("http://localhost:8080")
 			.allowedHeaders("*")
 			.allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
 		;
